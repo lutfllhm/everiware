d503065750a6726a7e893ced127dd856fe38491f
Lutfillah M <lutfillahmasduqi12@gmail.com>
2026-06-10 16:18:16 +0700

    mobile

--- iwareabsenku/lib/utils/constants.dart
+++ iwareabsenku/lib/utils/constants.dart
@@ -2,9 +2,9 @@ class AppConstants {
   // Ganti dengan IP PC kamu saat development
   // Contoh: 'http://192.168.120.64:5000/api'
   // Untuk production: 'https://yourdomain.com/api'
-  static const String baseUrl = 'http://192.168.120.223:5005/api';
-  static const String uploadsUrl = 'http://192.168.120.223:5005/uploads';
-  static const String webUrl = 'http://192.168.120.223:5173'; // URL frontend web
+  static const String baseUrl = 'https://everiware.iwareid.com/api';
+  static const String uploadsUrl = 'https://everiware.iwareid.com/uploads';
+  static const String webUrl = 'https://everiware.iwareid.com'; // URL frontend web
 
   static const String appName = 'Everiware';
   static const String appVersion = '1.0.0';
