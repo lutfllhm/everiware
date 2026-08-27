6a2d3a8a115b2f78e417d9fbb219e1a34bf1c5d6
Lutfillah M <lutfillahmasduqi12@gmail.com>
2026-08-27 13:15:25 +0700

    request

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -14,9 +14,10 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response?.status === 401) {
+    if (err.response?.status === 401 && window.location.pathname !== '/login') {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
+      localStorage.removeItem('iware-auth');
       window.location.href = '/login';
     }
     return Promise.reject(err);
